8c2769ccc25ba64a6f549ad61946bf500fe4ffcf
Jingyu Xiang <jxiang2@ualberta.ca>
2021-10-18 17:21:50 -0600

    author model

--- sdapis/models.py
+++ sdapis/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+import uuid
 
-# Create your models here.
+
+def uuid_hex():
+    return uuid.uuid4().hex
+
+class Author(AbstractUser):
+    email = models.EmailField(max_length=100, unique=True)
+    username = models.CharField(max_length=100)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    authorID = models.CharField(unique=True, default=uuid_hex, editable=False, max_length=100)
+    github = models.CharField(max_length=200, blank=True)
+
+    USERNAME_FIELD = 'email' # use email to login
+    REQUIRED_FIELDS = ['username']
+
+    def get_id(self):
+        return settings.HOST_URL + "author/" + self.authorID
+
+    def get_host(self):
+        return settings.HOST_URL
+
+    def get_type(self):
+        return "author"
