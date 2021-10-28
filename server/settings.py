86c1db83fb68657e9ee11fb2007812075e6c7610
Jingyu Xiang <jxiang2@ualberta.ca>
2021-10-28 10:37:25 -0600

    deploy frontend

--- server/settings.py
+++ server/settings.py
@@ -141,15 +141,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
-REACT_APP_DIR = os.path.join(BASE_DIR, 'src')
-
-STATICFILES_DIRS = [
-    os.path.join(REACT_APP_DIR, 'build', 'static'),
-]
 
 
 
